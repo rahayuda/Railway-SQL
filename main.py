2a5c4cba00dc0b92e087a089150407b0a0c68b17
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2025-04-05 10:23:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return '👋 Hello, World from Railway!'
+    return "Hello world, welcome to Railway!"
